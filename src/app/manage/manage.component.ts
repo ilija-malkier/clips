ac6783df0363c8e6f8b95ced15085aea5f0e8369
ilija-malkier <ilija.radojkovic@malkier.rs>
2023-07-13 10:18:32 +0200

    g

--- src/app/manage/manage.component.ts
+++ src/app/manage/manage.component.ts
@@ -82,4 +82,11 @@ export class ManageComponent implements OnInit,OnDestroy{
   }
 
 
+  async copyToClipboard($event: MouseEvent, docID: string | undefined) {
+    $event.preventDefault()
+    if(!docID) return
+    const url=`${location.origin}/clip/${docID}`;
+    await navigator.clipboard.writeText(url);
+    alert('Link Copied!')
+  }
 }
